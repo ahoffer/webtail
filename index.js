4bc9783c4286236ab8c185cf0fc1a20c659e6e95
Aaron Hoffer <github@hoffer.org>
2016-01-30 22:35:11 -0700

    This branch explores using vanilla node.js, without express or socket.io to stream a file to the client. Eventually, I would like to have all the different techniques available in one node instance, but served up on different ports.

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ var port = 8080;
 var fname = '/tmp/prbs/last.log';
 
 //Includes
-var app = require('express')()
-var http = require('http').Server(app)
+//var app = require('express')()
+var http = require('http')
 var io = require('socket.io')(http)
+var fs = require('fs')
 
 // The fs library does not keep reading the file once the
 // file descriptor is created; as soon as createReadStream
@@ -24,37 +25,54 @@ var io = require('socket.io')(http)
 var Tail = require('always-tail2')
 
 //Root path serves up the whole file in a request.
-app.get('/', function(req, res) {
-  res.sendFile(fname)
-});
+//app.get('/', function(req, res) {
+//  res.sendFile(fname)
+//});
 
 //Register the tail endpoint
-app.get('/tail', function(req,res) {
-  res.sendFile(__dirname + '/index.html')
-});
+//app.get('/tail', function(req,res) {
+//  res.sendFile(__dirname + '/index.html')
+//});
 
 //Register the Websocket handler
-io.on('connection', function(socket) {
-  console.error('CONNECTION')
+io.on('connection', function (socket) {
+    console.error('CONNECTION')
 
-  //Stream must be created inside the io.on scope
-  //var readStream = ts.createReadStream(fname)
-  var readStream = new Tail(fname, '\n')
+    //Stream must be created inside the io.on scope
+    //var readStream = ts.createReadStream(fname)
+    var readStream = new Tail(fname, '\n')
 
-  //Squirt a chunk of data to the client
-  readStream.on('line', function(line) {
-    socket.emit('line', line);
-  });
+    //Set up an HTML document to display the lines
+    res.write("<style>li {list-style: none;} </style><ul>");
+    //Squirt a chunk of data to the client
+    readStream.on('line', function (line) {
+        socket.emit('line', '<li>' + line + '</li>');
+    });
 
-  //Some streaming libraries throw the 'end' event.
-  //TODO: Enhance always-tail2 to throw close or end event.
-  readStream.on('end', function() {
-    socket.emit('end');
-  });
+    //Some streaming libraries throw the 'end' event.
+    //TODO: Enhance always-tail2 to throw close or end event.
+    readStream.on('end', function () {
+        socket.emit('end');
+    });
+});
 
+var server = http.createServer(function (req, res) {
+    fname = req.url;
+    console.error("GET " + fname);
+    //stream = fs.createReadStream(__dirname + '/index.html');
+    //res.writeHead(200, {
+    //    'Content-Type': 'text/html'
+    //});
+    //stream = fs.createReadStream(fname);
+
+//    res.writeHead(200, {
+//        'Content-Type': 'text/html'
+//    });
+//    res.write("<h1>" + fname + "</h1>");
+//    stream.pipe(res);
 });
 
 //Start Web server
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+server.listen(port, function () {
+    console.log('listening on *:' + port);
 });
